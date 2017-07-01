fe1fe0bedb5413f21b1a65e7294185700be8cf2d
Carlos Vargas <kr-lo-ss@hotmail.com>
2017-07-01 23:11:08 +0200

    reparin the list emails

--- includes/app/admin/templates/tpl_listemails.php
+++ includes/app/admin/templates/tpl_listemails.php
@@ -5,9 +5,24 @@
 	$wpemails_cpve_pass = isset($wpemails_cpve_options['txtpassword']) ? $wpemails_cpve_options['txtpassword'] : '';
 	//llamamos la clase, autenticando y mostrando todos los correos 
 	$cpmm = new cPanelMailManager($wpemails_cpve_user, $wpemails_cpve_pass, $wpemails_cpve_host);
+
+	//Separamos los datos del dominio
+	$host_ex = explode(".", $wpemails_cpve_host);
+	$host_ex[3] = (isset($host_ex[3]) and !empty($host_ex[3])) ? ".".$host_ex[3] : "";
+	$last_part_email ="@".$host_ex[1].".".$host_ex[2].$host_ex[3];
+
+
 	$data = $cpmm->listEmails();
-	print_r($data);
-	/*$cad = "";
+
+	function compare_domain($e, $e2){
+		$part_email = explode("@", $e);
+		if("@".$part_email[1] == $e2){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
 	echo '<table>';
 	echo '
 	<tr>
@@ -16,7 +31,7 @@
 		<th>--</th>
 	</tr>';
 		for($i=0; $i < count($data); $i++){
-			if(verify($data[$i]['email'], $last_part_email)){
+			if(compare_domain($data[$i]['email'], $last_part_email)){
 				$e_separe = explode("@", $data[$i]['email']);
 				$cad.="<tr>";
 				$cad.="<td>".$data[$i]['email']."</td>";
@@ -26,7 +41,9 @@
 				$cad.="</tr>";
 		}
 	}
-	echo '</table>';*/
+	print $cad;
+	echo '</table>';
+
 
 	
 
