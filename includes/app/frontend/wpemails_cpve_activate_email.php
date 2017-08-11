5fb18ea8bcdbc776b06ce92774a3538264239a85
avargas <avargas@frontuari.com>
2017-08-11 00:19:45 -0400

    Arreglando archivo para axtivacion de cuenta corporativa desde correo

--- includes/app/frontend/wpemails_cpve_activate_email.php
+++ includes/app/frontend/wpemails_cpve_activate_email.php
@@ -15,12 +15,10 @@
 		$wpemailscpve_front_options = $get_settings->wpemails_cpve_checkoptions();
 		$wpemails_cpve_front_estatuspost = get_post_status($_GET['post']);
 		$wpemails_cpve_front_data = get_post_meta($_GET['post'],'wpemails_cpve_cpt_options');
-		print_r($wpemails_cpve_front_data);
-
-		/*if($wpemails_cpve_front_estatuspost=='pending'){
+		if($wpemails_cpve_front_estatuspost=='pending'){
 			//condicion para saber si aceptamos el email
 			$cpmm_front = new cPanelMailManager($wpemailscpve_front_options['user'], $wpemailscpve_front_options['pass'], $wpemailscpve_front_options['host']);
-			if($cpmm_front->createEmail($wpemails_cpve_front_data['wpemails_cpve_email_corporative'],$wpemails_cpve_front_data['wpemails_cpve_email_password'],$wpemails_cpve_front_data['wpemails_cpve_plan'])){
+			if($cpmm_front->createEmail($wpemails_cpve_front_data[0]['wpemails_cpve_email_corporative'],$wpemails_cpve_front_data[0]['wpemails_cpve_email_password'],$wpemails_cpve_front_data[0]['wpemails_cpve_plan'])){
 					echo 'CORREO ACTIVADO SATISFACTORIAMENTE';
 					 $my_post = array(
 					 	'ID' => $_GET['post'],
@@ -30,6 +28,6 @@
 			}else{
 				echo 'OCURRIO UN ERROR AL ACTIVAR CORREO';
 			}
-		}*/
+		}
 	}
 
